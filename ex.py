2b891cbda6f41d97c439e95dcd1d5bc9af3ee93e
Algeek <allarassemchristian0@gmail.com>
2026-08-02 22:06:26 +0100

    change ex.py to mario

--- ex.py
+++ ex.py
@@ -1,5 +1,12 @@
-n = int(input("Number : "))
-print("Multiplication by  ",n);
-for i in range(1, 12):
-    print(n,"*",i, "=", n*i) 
+n = int(input("Height : "))
+for i in range(1,n + 1):
+    s = 0
+    for j in range(n):
+        if s == (n - i):
+            print("*", end="")
+        else:
+            print(" ", end="")
+            s = s + 1
+    print("") 
+    
 
